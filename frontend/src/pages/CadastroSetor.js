1d57b0913901dbbaadd625a0ec4bb465ef882cea
Victor Kühn <victorkuhn.dev@gmail.com>
2023-06-21 19:03:52 -0300

    Remoção de classes desnecessárias e duplicidades.

--- frontend/src/pages/CadastroSetor.js
+++ frontend/src/pages/CadastroSetor.js
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import Header from '../componentes/Header';
 import SideBar from '../componentes/SideBar';
-import '../styles/CadastroSetor.css';
 
 const CadastroSetor = () => {
   const navigate = useNavigate();
--- frontend/src/pages/Setor.js
+++ frontend/src/pages/Setor.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import axios from 'axios';
-import '../styles/Setor.css';
 import Header from '../componentes/Header';
 import SideBar from '../componentes/SideBar';
 import '../utils/locales';
